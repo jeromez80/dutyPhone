866d3e160f95b5e8386e68a8bd262d912c4a4beb
jeromez80 <jeromez80@users.noreply.github.com>
2015-09-09 22:43:45 +0800

    Create index.php

---
+++ www/index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title>Send SMS Form</title>
+</head>
+<body>
+<form method="POST" action="sendSMS.php">
+<input type="radio" name="desttype" value="dutynum" checked>Current DutyNumber - WhatsApp Only</input>
+<br>
+<input type="radio" name="desttype" value="destsms">Specific destination by SMS</input>
+<input type="radio" name="desttype" value="destwa">Specific destination by WhatsApp</input>
+<br>
+Specific Destination Number: <input type="text" name="destnumber" value="+65">
+<br>
+<input type="radio" name="desttype" value="destwagc">Whatsapp GroupChat</input>
+<hr>
+<input type="text" name="destMsg" value="text">
+<input type="submit">
+</form>
+</body>
+</html>
