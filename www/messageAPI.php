a77c6bb8536ac1d40abaa87dfa2738658a378734
jeromez80 <jeromez80@users.noreply.github.com>
2015-09-09 22:43:05 +0800

    Update messageAPI.php

--- www/messageAPI.php
+++ www/messageAPI.php
@@ -1,6 +1,36 @@
 <?php
-$PATH = '/var/www/jobs/';
-echo "OK\n";
-//echo $_POST["timestamp"] . " : " . $_POST["temp"] . " : " . $_POST["humidity"] . " : " . $_POST["destnumber"] . " ->" . $_POST["destMsg"];
-file_put_contents($PATH.md5(microtime()).'.msg', $_POST["destnumber"] . "\n" . $_POST["destMsg"]);
+$PATH1 = '/var/www/jobs/';
+$PATH2 = '/var/www/jobswa/';
+$DUTYNUM = '/root/mcmodem/dutynumber.txt';
+
+$destnumber = $_POST["destnumber"];
+$destMsg = $_POST["destMsg"];
+$sendsms = 0;
+$sendwa = 0;
+
+$desttype = $_POST["desttype"];
+
+if ($desttype=='dutynum') {
+	$sendwa = 1;
+	$destnumber = 'DUTYNUM';
+}
+else if ($desttype=='destsms') {
+	$sendsms = 1;
+}
+else if ($desttype=='destwa') {
+	$sendwa = 1;
+}
+else if ($desttype=='destwagc') {
+	$destnumber = '+YOURCHATNUMBERHERE';
+	$sendwa = 1;
+}
+
+	if ($sendsms==1) {
+	file_put_contents($PATH1.md5(microtime()).'.msg', $destnumber . "\n" . $destMsg);
+	}
+	if ($sendwa == 1) {
+	file_put_contents($PATH2.md5(microtime()).'.msg', $destnumber . "\n" . $destMsg);
+	}
+	echo "OK - sent!\n";
+
 ?>
