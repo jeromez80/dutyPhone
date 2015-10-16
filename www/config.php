3f2e30060cdfed8ddbc95e0a837f383f6340cae8
jeromez80 <jeromez80@users.noreply.github.com>
2015-10-17 01:21:36 +0800

    Update config.php

--- www/config.php
+++ www/config.php
@@ -4,3 +4,17 @@ $PATH1 = '/var/www/html/jobs/';
 $PATH2 = '/var/www/html/jobswa/';
 $DUTYNUM = '/usr/local/src/dutyPhone/dutynumber.txt';
 $CONFIGINI = '/usr/local/src/dutyPhone/config.ini';
+
+session_start();
+ob_start();
+define('DB_HOSTNAME','localhost');
+define('DB_USERNAME','');
+define('DB_PASSWORD','');
+define('DB_DATABASENAME','');
+define('Suffix','');
+$connect = mysql_connect(DB_HOSTNAME,DB_USERNAME,DB_PASSWORD);
+if(!$connect)
+{
+die(mysql_error());
+}
+mysql_select_db(DB_DATABASENAME,$connect);
