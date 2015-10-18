79f1b95cba4311c51d69b27b35b703e00310c7a2
JT <support@geektronics.sg>
2015-10-18 18:56:47 -0400

    Messages now come from database.

--- www/index.php
+++ www/index.php
@@ -229,9 +229,24 @@ $staticIP = file_get_contents('setIP.txt');
       <div class="large-12 medium-12 columns">
         <div class="panel">
         <h3>Message Log</h3>
-        <pre>
-        <?php echo file_get_contents('/usr/local/src/logs/sms-messages.txt'); ?>
-        </pre>
+<?php
+$select = "select timestamp, sender, receiver, message from `messages` order by id desc";
+$query = mysql_query($select);
+while($row=mysql_fetch_array($query))
+{
+	echo '
+		<div class="panel">
+		<div class="row">
+		<div class="large-4 medium-4 columns"><label>Date: '.$row[0].'</label></div>
+		<div class="large-4 medium-4 columns"><label>From: '.$row[1].'</label></div>
+		<div class="large-4 medium-4 columns"><label>To: '.$row[2].'</label></div>
+		</div><div class="row">
+		<div class="large-12 medium-12 columns"><label>Message: '.$row[3].'</label></div>
+		</div></div>
+	';
+}
+
+?>
         </div>
       </div>
     </div>
