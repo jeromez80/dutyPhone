42d6a469abfcda7316241c85382377ed0a4242a9
JT <support@geektronics.sg>
2015-10-18 15:09:39 -0400

    Removed unnecessary settings.

--- waListen/config.php
+++ waListen/config.php
@@ -1,9 +1,4 @@
 <?php
-//Selected group-chat IDs
-$gcListFile=something.txt;
-$gcCount=2;
-$groupchatID1=20394875208345;
-$groupchatID2=20354765865883;
 $poll_dir = '/var/www/html/jobswa/';
 $complete_dir = '/usr/local/src/logs/completed-wa/';
 
@@ -18,4 +13,3 @@ if(!$connect)
 die(mysql_error());
 }
 mysql_select_db(DB_DATABASENAME,$connect);
-echo 'DB Connected';
