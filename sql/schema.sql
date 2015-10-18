4752d956c34cdb5da48e81a00bf318782cf708b4
jeromez80 <jeromez80@users.noreply.github.com>
2015-10-18 19:11:39 +0800

    Create schema.sql

---
+++ sql/schema.sql
@@ -0,0 +1,109 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.15
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Oct 18, 2015 at 07:10 PM
+-- Server version: 5.5.44-MariaDB
+-- PHP Version: 5.4.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `smartMessage`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data`
+--
+
+CREATE TABLE IF NOT EXISTS `data` (
+  `username` text,
+  `password` text,
+  `nickname` text,
+  `login` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `group_details`
+--
+
+CREATE TABLE IF NOT EXISTS `group_details` (
+  `id` int(100) NOT NULL,
+  `group_names` varchar(100) NOT NULL,
+  `group_code` varchar(100) NOT NULL,
+  `status` varchar(15) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mmg_phone_numbers`
+--
+
+CREATE TABLE IF NOT EXISTS `mmg_phone_numbers` (
+  `id` int(100) NOT NULL,
+  `sms_message_center` varchar(25) NOT NULL,
+  `current_duty_number` varchar(25) NOT NULL,
+  `last_incoming_message` varchar(25) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `superviser_number`
+--
+
+CREATE TABLE IF NOT EXISTS `superviser_number` (
+  `id` int(100) NOT NULL,
+  `number` varchar(25) NOT NULL,
+  `number_type` varchar(50) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `group_details`
+--
+ALTER TABLE `group_details`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `group_code` (`group_code`);
+
+--
+-- Indexes for table `mmg_phone_numbers`
+--
+ALTER TABLE `mmg_phone_numbers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `superviser_number`
+--
+ALTER TABLE `superviser_number`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `group_details`
+--
+ALTER TABLE `group_details`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `mmg_phone_numbers`
+--
+ALTER TABLE `mmg_phone_numbers`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `superviser_number`
+--
+ALTER TABLE `superviser_number`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
