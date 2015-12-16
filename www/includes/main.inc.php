ee1f0f973c11ada1f287cdd239ed5635785979da
root <root@smartmessage.beta.sg>
2015-12-16 15:56:50 +0000

    remove file - from codes restructuring

--- www/includes/main.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-require(__DIR__."/../functions/ulogin.php");
-require(__DIR__."/../config/config.inc.php");
-require(__DIR__."/../functions/ConfigData.php");
-require(__DIR__."/../functions/actions.php");
-?>
-
-
