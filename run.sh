f0543431870beacb1f19d936fe6b035d1266024d
root <JacksonTan@gmail.com>
2016-06-10 00:15:18 +0800

    Run.sh - New centralized job file.

---
+++ run.sh
@@ -0,0 +1,5 @@
+cd /usr/local/src/dutyPhone
+/usr/local/src/dutyPhone/scripts/checkReboot.sh >/dev/null 2>&1
+/usr/local/src/dutyPhone/processSMS2 >/dev/null 2>&1
+/usr/local/src/dutyPhone/mail2msg >/dev/null 2>&1
+/usr/local/src/dutyPhone/syslogFilter >/dev/null 2>&1
